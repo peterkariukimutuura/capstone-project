6dab111e083579853373b2d43cb5567be6b7a60f
peterkariukimutuura <kariukipeter30@gmail.com>
2018-11-06 21:02:18 +0300

    small changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,10 +56,10 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
+{{-- 
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
                                     {{ __('Forgot Your Password?') }}
-                                </a>
+                                </a> --}}
                             </div>
                         </div>
                     </form>
